ed5ac0bd9b906c541aba5f3a78f59e94986820f2
asmarcd <asmarcd@gmail.com>
2021-03-09 11:10:29 -0800

    user routes and pseudocode for needed ticket routes

--- routes/Tickets.js
+++ routes/Tickets.js
@@ -13,52 +13,65 @@ router.get('/tickets', (req, res) => {
 });
 
 // create a new ticket
-router.post('/tickets', (req, res) => {
-    db.Ticket.create({
-        title: req.body.title,
-        type: req.body.type,
-        description: req.body.description,
-        priorty: req.body.priorty,
-        owner: req.body.owner
-    })
-    res.status(200).send("New Ticket Added")
+router.post('/tickets', async (req, res) => {
+    await db.Ticket.create(
+        {
+            title: req.body.title,
+            type: req.body.type,
+            description: req.body.description,
+            priorty: req.body.priorty,
+            owner: req.body.owner
+        }
+    )
+    await res.status(200).send("New Ticket Added")
 });
+
+// find a specific ticket
+
+// search tickets by title
+
+// find all tickets of a particular type
+
+// find all tickets with particular owner
 
 // update existing ticket
 router.put('/tickets/:id', (req, res) => {
-    db.Ticket.update({
-        title: req.body.title,
-        type: req.body.type,
-        description: req.body.description,
-        priorty: req.body.priorty,
-        owner: req.body.owner
-    },
+    db.Ticket.update(
+        {
+            title: req.body.title,
+            type: req.body.type,
+            description: req.body.description,
+            priorty: req.body.priorty,
+            owner: req.body.owner
+        },
         {
             where: {
                 id: req.params.id
             }
-        }).then(updatedTicket => {
-            if (updatedTicket.changedRows == 0) {
-                return res.status(404).end();
-            } else {
-                res.status(200).end();
-            }
-        })
-});
-
-// delete existing ticket
-router.delete('/tickets/:id', (req, res) => {
-    db.Ticket.destroy({
-        where: {
-            id: req.params.id
         }
-    }).then(deletedTicket => {
-        if (deletedTicket.changedRows == 0) {
+    ).then(updatedTicket => {
+        if (updatedTicket.changedRows == 0) {
             return res.status(404).end();
         } else {
             res.status(200).end();
         }
     })
+});
+
+// delete existing ticket
+router.delete('/tickets/:id', (req, res) => {
+    db.Ticket.destroy(
+        {
+            where: {
+                id: req.params.id
+            }
+        }).then(deletedTicket => {
+            if (deletedTicket.changedRows == 0) {
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+        })
 });
 
 module.exports = router;
--- routes/User.js
+++ routes/User.js
@@ -1,8 +1,77 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models');
 
-router.get('/user', (req,res) => {
-    res.send({data: "users placeholder"})
+// show all users
+router.get('/users', (req, res) => {
+    db.User.findAll()
+        .then(allUsers => {
+            res.send(allUsers)
+        })
 });
+
+// show a specific user
+router.get('/users/:id', (req, res) => {
+    db.User.findOne(
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    ).then(user => {
+        res.send(user)
+    }).catch(console.log('Error'))
+});
+
+// create a new user
+router.post('/users', async (req, res) => {
+    await db.User.create(
+        {
+            name: req.body.name,
+            username: req.body.username,
+            password: req.body.password
+        }
+    )
+    await res.status(200).send('New User Added')
+});
+
+// update existing user
+router.put('/users/:id', (req, res) => {
+    db.User.update(
+        {
+            name: req.body.name,
+            username: req.body.username,
+            password: req.body.password
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    ).then(updatedUser => {
+        if (updatedUser.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    })
+});
+
+// delete existing user
+router.delete('/users/:id', (req, res) => {
+    db.User.destroy(
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    ).then(deletedUser => {
+        if (deletedUser.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    })
+})
 
 module.exports = router;
