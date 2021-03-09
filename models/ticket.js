a4cc5086d78b388ea5b41377d72b6bdf6792f854
asmarcd <asmarcd@gmail.com>
2021-03-09 15:50:13 -0800

    adjusted ticket model dates

--- models/ticket.js
+++ models/ticket.js
@@ -1,5 +1,3 @@
-const moment= require('moment');
-
 module.exports = function (sequelize, DataTypes) {
     const Ticket = sequelize.define("Ticket", {
         id: {
@@ -30,18 +28,6 @@ module.exports = function (sequelize, DataTypes) {
         owner: {
             type: DataTypes.INTEGER,
             allowNull: true
-        },
-        created_at: {
-            type: DataTypes.DATE,
-            get() {
-                return moment(this.getDataValue('created_at')).format('DD/MM/YYYY h:mm:ss');
-            }
-        },
-        updated_at: {
-            type: DataTypes.DATE,
-            get() {
-                return moment(this.getDataValue('updated_at')).format('DD/MM/YYYY h:mm:ss');
-            }
         }
     })
     return Ticket;
